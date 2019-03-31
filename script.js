41168dce053a67985066c61d5c7a679523e2524b
ajrulsapeken <ajrulsapeken@gmail.com>
2019-03-31 14:54:09 +0700

    Belajar JS

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("Assalamualaikum External ")
+document.getElementById("heading2").innerHTML="Assalamualaikum";
+
+var nama="ajrul";
+console.log(name);
+
+var lastName="Sapeken";
+
+var Usia=17;
+console.log(Usia);
+
+var fullNama =name + ' ' +lastName;
+console.log(fullNama);
+
+var job="Trainer";
+console.log(job);
+
+var bio =" Nama Saya " + fullNama + " " + "Usia" + " " + Usia + " "+"Tahun" + " "+"Saya Sebagai" + " "+job;
+console.log(bio);
+document.getElementById("t1").innerHTML=bio;
+// Penjumlahan Dalam JS Sama Dengan Perkalian Pembagian */
+var t = "Total";
+var a= 100;
+var b=200;
+var c= t+" "+(a+b);
+console.log(c);
+document.getElementById("t2").innerHTML=c;
+
+var name=prompt("Apa aja boleh ")
+alert(name);
+console.log(name);
+
+
+var car = "black";
+if (car=='car'? console.log("is My Car"):console.log("is not"));
+car == 'black' ? console.log("is My car"): console.log("is not")  
